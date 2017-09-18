8c41970caccd4e519795dff726c9b9f62814544b
Peter Tschipper <peter.tschipper@gmail.com>
2017-09-18 16:46:27 -0700

    fixes for coins.h

--- src/coins.h
+++ src/coins.h
@@ -140,35 +140,6 @@ private:
     uint256 hashBlock;
 };
 
-/*
-struct CCoinsStats
-{
-public:
-    CCoinsViewCursor(const uint256 &hashBlockIn): hashBlock(hashBlockIn) {}
-    virtual ~CCoinsViewCursor() {}
-
-    virtual bool GetKey(COutPoint &key) const = 0;
-    virtual bool GetValue(Coin &coin) const = 0;
-    virtual unsigned int GetValueSize() const = 0;
-
-    virtual bool Valid() const = 0;
-    virtual void Next() = 0;
-
-    //! Get best block at the time this cursor was created
-    const uint256 &GetBestBlock() const { return hashBlock; }
-private:
-    uint256 hashBlock;
-    uint64_t nTransactions;
-    uint64_t nTransactionOutputs;
-    uint64_t nSerializedSize;
-    uint256 hashSerialized;
-    CAmount nTotalAmount;
-
-    CCoinsStats() : nHeight(0), nTransactions(0), nTransactionOutputs(0), nSerializedSize(0), nTotalAmount(0) {}
-};
-
-*/
-
 /** Abstract view on the open txout dataset. */
 class CCoinsView
 {
@@ -190,9 +161,6 @@ public:
     //! The passed mapCoins can be modified.
     virtual bool BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlock, size_t &nChildCachedCoinsUsage);
 
-    //! Calculate statistics about the unspent transaction output set
-    virtual bool GetStats(CCoinsStats &stats) const;
-
     //! As we use CCoinsViews polymorphically, have a virtual destructor
     virtual ~CCoinsView() {}
 };
@@ -210,10 +178,9 @@ public:
     bool HaveCoin(const COutPoint &outpoint) const override;
     uint256 GetBestBlock() const override;
     void SetBackend(CCoinsView &viewIn);
-    bool GetStats(CCoinsStats &stats) const;
     bool BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlock, size_t &nChildCachedCoinsUsage) override;
-    CCoinsViewCursor *Cursor() const override;
-    size_t EstimateSize() const override;
+    CCoinsViewCursor *Cursor() const;
+    size_t EstimateSize() const;
 };
 
 
